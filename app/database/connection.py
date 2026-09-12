cad1b0fe6f1f29fea6347780ffd72081166a3e02
Raghavendra B C <raghavendrabc.02@gmail.com>
2026-09-12 20:42:21 +0530

    Add database migration management

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,19 +1,20 @@
 import os
+
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.models.article import Article
-from app.database.base import Base
-load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
 
+from app.database.base import Base
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
-Base.metadata.create_all(bind=engine)
 
 
 def get_db():
@@ -22,3 +23,4 @@ def get_db():
         yield db
     finally:
         db.close()
+        
